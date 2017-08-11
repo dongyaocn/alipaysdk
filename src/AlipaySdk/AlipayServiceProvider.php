40def0e32ad9c8a235f9e847e663878985fd2735
董垚 <dong_yao_cn@163.com>
2017-08-11 17:05:45 +0800

    增加ServiceProvider
    Signed-off-by: 董垚 <dong_yao_cn@163.com>

---
+++ src/AlipaySdk/AlipayServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlipaySdk;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlipayServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
